dd488a395335905bb33d4a2f37a4b7468b801086
Marcus Grant <marcusfg@gmail.com>
2022-04-02 17:11:16 +0200

    Add UserScript metadata

---
+++ userscript.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         r/Place Overlay
+// @namespace    https://github.com/marcus-grant/place-overlay
+// @version      1.0.1
+// @description  A visual overlay to show errors in tile colors of a desired image in r/place
+// @author       github.com/marcus-grant
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @grant        none
+// @license      GPL-3.0
+// ==/UserScript==
